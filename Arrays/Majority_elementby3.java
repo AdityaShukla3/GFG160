10685d2297097fea08984e134a37be729b8c512d
Aditya Shukla <adityashukla2036@gmail.com>
2025-12-11 14:52:45 +0530

    day6

---
+++ Arrays/Majority_elementby3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // Code here
+        int n=arr.length;
+        Map<Integer,Integer> map=new HashMap<>();
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue()>n/3){
+                list.add(e.getKey());
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
